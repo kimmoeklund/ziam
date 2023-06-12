4fce073756a5a21472554553365ecf29ccef3b74
Kimmo Eklund <kimmo@kimmoeklund.fi>
2023-06-12 11:11:51 +0300

    Add missing Api

---
+++ src/main/scala/fi/kimmoeklund/ziam/Api.scala
@@ -0,0 +1,18 @@
+package fi.kimmoeklund.ziam
+
+import zio._
+import zio.http._
+import zio.metrics._
+import fi.kimmoeklund.service.UserRepository
+import fi.kimmoeklund.domain._
+import zio.json._
+
+object ZiamApi:
+  def apply(): HttpApp[UserRepository, Nothing] = Http.collectZIO[Request] {
+    case Method.GET -> !! / "users" => 
+      val effect = for {
+        users <- UserRepository.getUsers()
+      } yield users
+      effect.foldZIO(_ => ZIO.succeed(Response.status(Status.BadRequest)), users => ZIO.succeed(Response.json(users.toJson)))      
+  }
+
