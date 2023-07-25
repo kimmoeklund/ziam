8bc0f1f59eae41c96f9239589d89061aabac542f
Kimmo Eklund <kimmo@kimmoeklund.fi>
2023-07-25 18:38:15 +0300

    sqlite refactor and cli started (wip)

--- jvm/src/main/scala/fi/kimmoeklund/ziam/Main.scala
+++ jvm/src/main/scala/fi/kimmoeklund/ziam/Main.scala
@@ -21,16 +21,20 @@ import scala.util.Try
 object Main extends ZIOAppDefault:
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] =
     Runtime.removeDefaultLoggers >>> SLF4J.slf4j
-  private val dataSourceLayer = Quill.DataSource.fromPrefix("ziam-db")
-  private val postgresLayer =
-    Quill.Postgres.fromNamingStrategy(NamingStrategy(SnakeCase, Escape))
+  val sqliteDs = Quill.DataSource.fromPrefix("ziam-sqlite-db")
+  val sqliteQuill = Quill.Sqlite.fromNamingStrategy(NamingStrategy(SnakeCase, Escape))
+//  private val dataSourceLayer = Quill.DataSource.fromPrefix("ziam-db")
+//  private val postgresLayer =
+  Quill.Postgres.fromNamingStrategy(NamingStrategy(SnakeCase, Escape))
   private val passwordFactory: ZLayer[Any, Throwable, Argon2PasswordFactory] = ZLayer.scoped {
     for {
       factory <- ZIO.attempt(Argon2PasswordFactory(parallelism = 1, memory = 100 * 1024))
     } yield (factory)
   }
-  private val repoLayer = UserRepositoryLive.layer
+//  private val repoLayer = UserRepositoryLive.pgLayer
+  private val repoLayer = UserRepositoryLive.sqliteLayer
   private val requestCounter = Metric.counter("requestCounter").fromConst(0)
+  // todo: reactor to ZIO-config
   private val cookieSecret = "mysecret"
   private val domain = "localhost"
   private val authCookie = Cookie.Response("ziam", "", Some(domain), None, false, true)
@@ -65,5 +69,5 @@ object Main extends ZIOAppDefault:
           ++ RolesPage.httpValue
           ++ OrganizationsPage.httpValue) @@ whenRequestZIO(invalidCookie)(basicAuthAndAddCookie) ++ ZiamApi()
       )
-      .provide(Server.default, dataSourceLayer, postgresLayer, passwordFactory, repoLayer)
+      .provide(Server.default, sqliteDs, sqliteQuill, passwordFactory, repoLayer)
   }
