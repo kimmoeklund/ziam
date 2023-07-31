f73be55a4f4ba57ad016c25a1ecb76c090c6b2ec
Kimmo Eklund <kimmo@kimmoeklund.fi>
2023-07-31 17:04:36 +0300

    add default databases directory for tests

--- jvm/src/main/scala/fi/kimmoeklund/service/DbManagementLive.scala
+++ jvm/src/main/scala/fi/kimmoeklund/service/DbManagementLive.scala
@@ -5,20 +5,17 @@ final class DbManagementLive extends DbManagement:
   override def provisionDatabase(dbName: String): IO[DbManagementError, Unit] =
     (for {
       config <- ZIO.config(DbConfig.config)
-      file <- ZIO.attempt({
+      file <- ZIO.attemptBlockingIO({
         val file = new java.io.File(s"${config.dbLocation}/$dbName.db")
         if file.createNewFile() then ZIO.succeed(file) else ZIO.fail(DbManagementError.DbAlreadyExists)
       })
       schema <- ZIO.attempt(scala.io.Source.fromResource("ziam_schema.sql").mkString)
-      _ <- ZIO.logInfo(schema)
       _ <- ZIO.attemptBlocking({
         val ds = new org.sqlite.SQLiteDataSource()
         ds.setUrl(s"jdbc:sqlite:${config.dbLocation}/$dbName.db")
         val conn = ds.getConnection()
-        println("foo")
         val stmt = conn.createStatement()
         val rs = stmt.executeUpdate(schema)
-        println("foo")
       })
       _ <- ZIO.logInfo("schema created")
     } yield ()).mapError({ case e: Throwable =>
