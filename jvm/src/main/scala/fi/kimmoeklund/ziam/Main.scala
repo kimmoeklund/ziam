d9483d5f0d11f3c4d00b0e13254975c6a60776e7
Kimmo Eklund <kimmo@kimmoeklund.fi>
2023-08-01 13:53:22 +0300

    Add LoginPage

--- jvm/src/main/scala/fi/kimmoeklund/ziam/Main.scala
+++ jvm/src/main/scala/fi/kimmoeklund/ziam/Main.scala
@@ -6,11 +6,13 @@ import fi.kimmoeklund.service.*
 import io.getquill.{Escape, SnakeCase}
 import zio.*
 import zio.http.*
-import zio.http.HttpAppMiddleware.{addCookie, basicAuthZIO, signCookies, whenRequestZIO, whenStatus}
+import zio.http.HttpAppMiddleware.{addCookie, basicAuthZIO, signCookies, whenRequestZIO, whenStatus, customAuthZIO, redirect}
 import zio.logging.backend.SLF4J
 import zio.metrics.*
 
 import java.io.File
+import java.time.Duration
+import fi.kimmoeklund.html.pages.LoginPage
 
 object Main extends ZIOAppDefault:
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] =
@@ -18,27 +20,14 @@ object Main extends ZIOAppDefault:
   private val requestCounter = Metric.counter("requestCounter").fromConst(0)
   // todo: reactor to ZIO-config
   private val cookieSecret = "mysecret"
-  private val domain = "localhost"
-  private val authCookie = Cookie.Response("ziam", "", Some(domain), None, false, true)
+  private val authCookie = Cookie.Response("ziam", "", None, Some(Root), false, true).sign(cookieSecret)
+  private val logoutCookie = Cookie.Response("ziam", "", None, Some(Root), false, true, Some(Duration.ZERO)).sign(cookieSecret)
   private val invalidCookie =
     (request: Request) => {
       val cookie =
         request.header(Header.Cookie).map(_.value.toChunk).getOrElse(Chunk.empty).find(_.name == authCookie.name)
       ZIO.succeed(if (cookie.isEmpty) then true else cookie.head.unSign(cookieSecret).isEmpty)
     }
-
-  private val basicAuthAndAddCookie =
-    basicAuthZIO(creds =>
-      for {
-        repo <- ZIO.serviceAt[UserRepository]("ziam")
-        result <- repo.get
-          .checkUserPassword(creds.uname, creds.upassword)
-          .tapError(e => ZIO.logInfo(e.toString))
-          .fold(_ => false, _ => true)
-      } yield (result)
-    ) >>> whenStatus(status => status != Status.Unauthorized)(
-      addCookie(authCookie) >>> signCookies(cookieSecret)
-    )
 
   private val scriptsAndMainPage = Http.collectHttp[Request] {
     case Method.GET -> Root / "scripts" =>
@@ -47,11 +36,11 @@ object Main extends ZIOAppDefault:
   }
 
   val site = Site.build("ziam")
-
-  val httpApps =
+  val loginPage = LoginPage("login", "logout", "ziam", authCookie, logoutCookie)
+  val httpApps = loginPage.loginApp ++ 
     (scriptsAndMainPage.withDefaultErrorResponse ++ site.httpValue) @@ whenRequestZIO(invalidCookie)(
-      basicAuthAndAddCookie
-    ) ++ ZiamApi()
+      redirect(URL(Root / "ziam" / "login"), false)
+    ) ++ ZiamApi() 
 
   def run = {
     Server
