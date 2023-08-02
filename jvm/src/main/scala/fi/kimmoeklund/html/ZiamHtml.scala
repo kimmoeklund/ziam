fa2f3ef9b41df73beaee99759c446afa2b7c1c9c
Kimmo Eklund <kimmo@kimmoeklund.fi>
2023-08-02 17:09:08 +0300

    Second version of ZiamHtml that supports parameter names

--- jvm/src/main/scala/fi/kimmoeklund/html/ZiamHtml.scala
+++ jvm/src/main/scala/fi/kimmoeklund/html/ZiamHtml.scala
@@ -4,56 +4,43 @@ import zio.http.html.Element.PartialElement
 import magnolia1.* 
 import zio.http.html.Dom
 
-extension [A: ZiamHtml](value: A) def wrapWith(element: PartialElement): List[Dom] = summon[ZiamHtml[A]](value, element)
+extension [A: ZiamHtml](value: A) def wrapWith(element: PartialElement): List[Dom] = summon[ZiamHtml[A]].wrapValueWith(element, value)
 
 trait ZiamHtml[A]:
-  def apply(a: A, element: PartialElement): List[Dom]
-//  def wrapValueWith(element: PartialElement): Seq[Dom]
-//  def wrapParametersWith(element: PartialElement): Seq[Dom]
+  def wrapValueWith(element: PartialElement, value: A): List[Dom]
+  def wrapParametersWith(element: PartialElement): List[Dom]
  
-object ZiamHtml extends Derivation[ZiamHtml]:
+object ZiamHtml extends AutoDerivation[ZiamHtml]:
 
-  override def split[T](ctx: SealedTrait[fi.kimmoeklund.html.ZiamHtml, T]): ZiamHtml[T] = (a, e) => ctx.choose(a) { sub => sub.typeclass(sub.value, e) }
+  inline def apply[A](using A: ZiamHtml[A]): ZiamHtml[A] = A
 
-  override def join[T](ctx: CaseClass[fi.kimmoeklund.html.ZiamHtml, T]): ZiamHtml[T] = (a, e) =>
-   ctx.params.foldLeft(List[Dom]()) { (acc, p) =>
-      acc ++ p.typeclass(p.deref(a), e)
-    }
+  override def split[A](ctx: SealedTrait[fi.kimmoeklund.html.ZiamHtml, A]): ZiamHtml[A] =  
+    new ZiamHtml[A]:
+      def wrapValueWith(element: PartialElement, value: A) = ctx.choose(value) { sub => List(element(sub.typeInfo.short)) } 
+      def wrapParametersWith(element: PartialElement) = List(element(ctx.typeInfo.short)) 
 
-  given ZiamHtml[String] = (s, e) => List(e(s))
-  given ZiamHtml[Int] = (i, e) => List(e(i.toString))
-  given ZiamHtml[java.util.UUID] = (u, e) => List(e(u.toString))
-  given [T: ZiamHtml]: ZiamHtml[Seq[T]] = (s, e) => s.toList.flatMap(summon[ZiamHtml[T]](_,e))
+  override def join[A](ctx: CaseClass[fi.kimmoeklund.html.ZiamHtml, A]): ZiamHtml[A] = 
+    new ZiamHtml[A]:
+      def wrapValueWith(element: PartialElement, value: A) = ctx.params.foldLeft(List[Dom]()) { (acc, p) => 
+        acc ++ p.typeclass.wrapValueWith(element, p.deref(value))
+      }
+      def wrapParametersWith(element: PartialElement) = ctx.params.map(p => element(p.label)).toList 
+      
 
-extension [A: Csv](value: A) def csv: List[String] = summon[Csv[A]](value)
-
-trait Csv[A]:
-  def apply(a: A): List[String]
-
-object Csv extends Derivation[Csv]:
-  def join[A](ctx: CaseClass[Csv, A]): Csv[A] = a =>
-    ctx.params.foldLeft(List[String]()) { (acc, p) =>
-      acc ++ p.typeclass(p.deref(a))
-    }
-
-  def split[A](ctx: SealedTrait[Csv, A]): Csv[A] = a => ctx.choose(a) { sub => sub.typeclass(sub.value) }
-
-  given Csv[String] = l => List(s"<td>${l}</td>")
-  given Csv[Int] = i => List(s"<td>${i.toString}</td>")
-  given Csv[Char] = c => List(s"<td>${c.toString}</td>")
-  given [T: Csv]: Csv[Seq[T]] = _.to(List).flatMap(summon[Csv[T]](_))
-
-
-
-
-
-//    new ZiamHtml[T]:
-//      def wrapValueWith(element: PartialElement) = ctx.params.foldLeft(List[Dom]()) { (acc, p) => acc ++ element(p.deref().toString()) }
-//      def wrapParametersWith(element: PartialElement) = ???
-//
-//  override def split[T](sealedTrait: SealedTrait[fi.kimmoeklund.html.ZiamHtml, T]): ZiamHtml[T] = ???
-//    new ZiamHtml[T]:
-//      def wrapValueWith(element: PartialElement) = ???
-//      def wrapParametersWith(element: PartialElement) = ???
-
+  given ZiamHtml[java.util.UUID] with {
+    def wrapValueWith(element: PartialElement, value: java.util.UUID) = List(element(value.toString))
+    def wrapParametersWith(element: PartialElement) = List(element("uuid")) 
+  }
+  given ZiamHtml[String] with {
+    def wrapValueWith(element: PartialElement, value: String) = List(element(value.toString))
+    def wrapParametersWith(element: PartialElement) = List(element("string"))
+  }
+  given ZiamHtml[Int] with {
+    def wrapValueWith(element: PartialElement, value: Int) = List(element(value.toString))
+    def wrapParametersWith(element: PartialElement) = List(element("int")) 
+  }
+  given [A: ZiamHtml]: ZiamHtml[Seq[A]] with {
+    def wrapValueWith(element: PartialElement, value: Seq[A]) = value.flatMap(summon[ZiamHtml[A]].wrapValueWith(element, _)).toList
+    def wrapParametersWith(element: PartialElement) = summon[ZiamHtml[A]].wrapParametersWith(element)
+  }
 
