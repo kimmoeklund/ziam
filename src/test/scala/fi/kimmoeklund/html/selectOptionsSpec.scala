ce71d37902d1bcbcfd68d113b9a80a8c24d13ce9
Kimmo Eklund <kimmo@kimmoeklund.fi>
2025-08-08 12:20:12 +0300

    Fix select options test

--- src/test/scala/fi/kimmoeklund/html/selectOptionsSpec.scala
+++ src/test/scala/fi/kimmoeklund/html/selectOptionsSpec.scala
@@ -6,7 +6,7 @@ object selectOptionsSpec extends ZIOSpecDefault {
   override def spec = suite("selectOptions")(
     test("it add query parameters for hx-get") {
       val selectOptions = selectOption("path", "name", Seq("1", "2")).encode.toString()
-      assertTrue(selectOptions.contains("hx-get=\"path?selected=1&ampselected=2\""))
+      assertTrue(selectOptions.contains("hx-get=\"path?selected=1&selected=2\""))
     }
   )
 }
